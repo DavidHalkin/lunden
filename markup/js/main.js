66184efd6db69a9b1e02cab15720b6ac28f4d9fb
DavidHalkin <northfire@mail.ru>
2017-11-27 12:59:56 +0200

    fix mob nav

--- markup/js/main.js
+++ markup/js/main.js
@@ -4,9 +4,4 @@ $(document).ready(function(){
         $("#nav").slideToggle("fast");
         $(this).toggleClass("active");
     });
-    // tags
-    //  $(".btn-mob-tags").click(function(){
-    //     $("#tags-nav").slideToggle("fast");
-    //     $(this).toggleClass("active");
-    // });
 });
